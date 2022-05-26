c179fb0f04e8e5ed5c422bf077e4e67fcde81c64
Abhyuday <abhyudayb@observe.ai>
2022-05-27 01:21:34 +0530

    creating stack profiles

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -18,36 +18,43 @@ const SCREEN_WIDTH = Dimensions.get("window").width;
 function Home(googleData) {
   const { email, given_name, name } = googleData.route.params;
 
-  return cats.map((cats, i) => {
-    return (
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ height: 60 }} />
       <View style={{ flex: 1 }}>
-        <View style={{ height: 60 }} />
-        <View style={{ flex: 1 }}>
-          <Animated.View
-            style={[
-              {
-                height: SCREEN_HEIGHT - 120,
-                width: SCREEN_WIDTH,
-                padding: 10,
-              },
-            ]}
-          >
-            <Image
-              style={{
-                flex: 1,
-                height: null,
-                width: null,
-                resizeMode: "cover",
-                borderRadius: 20,
-              }}
-              source={cats[0].uri}
-            />
-          </Animated.View>
-        </View>
-        <View style={{ height: 60 }} />
+        <Profile />
       </View>
+      <View style={{ height: 60 }} />
+    </View>
+  );
+}
+
+//animated cat render
+const Profile = () => {
+  return cats.map((cat, i) => {
+    return (
+      <Animated.View
+        key={i}
+        style={{
+          height: SCREEN_HEIGHT - 120,
+          width: SCREEN_WIDTH,
+          padding: 10,
+          position: "absolute",
+        }}
+      >
+        <Image
+          style={{
+            flex: 1,
+            height: null,
+            width: null,
+            resizeMode: "cover",
+            borderRadius: 20,
+          }}
+          source={cat.uri}
+        />
+      </Animated.View>
     );
   });
-}
+};
 
 export default Home;
