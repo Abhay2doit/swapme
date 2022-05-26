51eab8eb0a8c9a433d6a67b791fb139243fcf5d6
Abhyuday <abhyudayb@observe.ai>
2022-05-27 00:57:00 +0530

    home page view

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,16 +1,19 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, Text, View, Dimensions, Image } from "react-native";
+
+//env variable
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 function Home(googleData) {
   console.log(googleData);
   const { email, given_name, name } = googleData.route.params;
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Text>{email}</Text>
-      <Text>{given_name}</Text>
-      <Text>{name}</Text>
+    <View style={{ flex: 1 }}>
+      <View style={{ height: 60 }}></View>
+      <View style={{ flex: 1 }}>{renderUsers()}</View>
+      <View style={{ height: 60 }}></View>
     </View>
   );
 }
