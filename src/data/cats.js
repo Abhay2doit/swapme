8f8571823f332938a216a0aa524b433d5248a741
Abhyuday <abhyudayb@observe.ai>
2022-05-27 01:07:33 +0530

    mapping cat images to map function

--- src/data/cats.js
+++ src/data/cats.js
@@ -7,3 +7,4 @@ const cats = [
 ];
 
 export default cats;
+
--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -8,44 +8,46 @@ import {
   Animated,
 } from "react-native";
 
-//
+//importing image
+import cats from "../../data/cats";
 
 //env variable
 const SCREEN_HEIGHT = Dimensions.get("window").height;
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
 function Home(googleData) {
-  console.log(googleData);
   const { email, given_name, name } = googleData.route.params;
 
-  return (
-    <View style={{ flex: 1 }}>
-      <View style={{ height: 60 }} />
+  return cats.map((cats, i) => {
+    return (
       <View style={{ flex: 1 }}>
-        <Animated.View
-          style={[
-            {
-              height: SCREEN_HEIGHT - 120,
-              width: SCREEN_WIDTH,
-              padding: 10,
-            },
-          ]}
-        >
-          <Image
-            style={{
-              flex: 1,
-              height: null,
-              width: null,
-              resizeMode: "cover",
-              borderRadius: 20,
-            }}
-            source={Users[0].uri}
-          />
-        </Animated.View>
+        <View style={{ height: 60 }} />
+        <View style={{ flex: 1 }}>
+          <Animated.View
+            style={[
+              {
+                height: SCREEN_HEIGHT - 120,
+                width: SCREEN_WIDTH,
+                padding: 10,
+              },
+            ]}
+          >
+            <Image
+              style={{
+                flex: 1,
+                height: null,
+                width: null,
+                resizeMode: "cover",
+                borderRadius: 20,
+              }}
+              source={cats[0].uri}
+            />
+          </Animated.View>
+        </View>
+        <View style={{ height: 60 }} />
       </View>
-      <View style={{ height: 60 }} />
-    </View>
-  );
+    );
+  });
 }
 
 export default Home;
